38e2c4392d56a0ccae9ab623d525e63dece864d1
Daniel Bigos <daniel.bigos@icloud.com>
2025-08-13 13:43:01 +0200

    feat: add ERC-20 Metadata extension

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,36 @@
 #[macro_use]
 extern crate alloc;
 
+use alloc::string::String;
 use alloc::vec::Vec;
-
-use openzeppelin_stylus::token::erc20::{self, extensions::IErc20Burnable, Erc20, IErc20};
+use openzeppelin_stylus::token::erc20::{
+    self,
+    extensions::{Erc20Metadata, IErc20Burnable, IErc20Metadata},
+    Erc20, IErc20,
+};
+use openzeppelin_stylus::utils::introspection::erc165::IErc165;
 use stylus_sdk::{
-    alloy_primitives::{Address, U256},
+    alloy_primitives::{aliases::B32, Address, U256, U8},
     prelude::*,
 };
-
 // Define some persistent storage.
 // `Erc20Workshop` will be the entrypoint.
 #[entrypoint]
 #[storage]
 struct Erc20Workshop {
     erc20: Erc20,
+    metadata: Erc20Metadata,
 }
 
 /// Declare that `Erc20Workshop` is a contract with the following external methods.
 #[public]
-#[implements(IErc20<Error = erc20::Error>, IErc20Burnable<Error = erc20::Error>)]
-impl Erc20Workshop {}
+#[implements(IErc20<Error = erc20::Error>, IErc20Burnable<Error = erc20::Error>, IErc20Metadata, IErc165)]
+impl Erc20Workshop {
+    #[constructor]
+    pub fn constructor(&mut self, name: String, symbol: String) {
+        self.metadata.constructor(name, symbol);
+    }
+}
 
 #[public]
 impl IErc20 for Erc20Workshop {
@@ -70,5 +80,28 @@ impl IErc20Burnable for Erc20Workshop {
 
     fn burn_from(&mut self, account: Address, value: U256) -> Result<(), Self::Error> {
         self.erc20.burn_from(account, value)
+    }
+}
+
+#[public]
+impl IErc20Metadata for Erc20Workshop {
+    fn name(&self) -> String {
+        self.metadata.name()
+    }
+
+    fn symbol(&self) -> String {
+        self.metadata.symbol()
+    }
+
+    fn decimals(&self) -> U8 {
+        self.metadata.decimals()
+    }
+}
+
+#[public]
+impl IErc165 for Erc20Workshop {
+    fn supports_interface(&self, interface_id: B32) -> bool {
+        self.erc20.supports_interface(interface_id)
+            || self.metadata.supports_interface(interface_id)
     }
 }
